f99517e83d1e9bc025586f98e6043419b7c32937
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-08-02 23:10:15 +0300

    fix: remove progress bar after being finished

--- flutter_animations/lib/widgets/progress_bar.dart
+++ flutter_animations/lib/widgets/progress_bar.dart
@@ -1,52 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ProgressBar extends StatelessWidget {
+class ProgressBar extends StatefulWidget {
   const ProgressBar({
     super.key,
     required this.progressBarAnimation,
     required this.sizeAnimation,
-    required this.slideAnimation
+    required this.slideAnimation,
   });
   final Animation<double> progressBarAnimation;
   final Animation<double> sizeAnimation;
   final Animation<Offset> slideAnimation;
 
   @override
+  State<ProgressBar> createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  bool _isVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.progressBarAnimation.addListener(_checkVisibility);
+  }
+
+  @override
+  void dispose() {
+    widget.progressBarAnimation.removeListener(_checkVisibility);
+    super.dispose();
+  }
+
+  void _checkVisibility() {
+    if (widget.progressBarAnimation.value >= 200 && _isVisible) {
+      setState(() {
+        _isVisible = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: SlideTransition(
-        position: slideAnimation,
-        child: Stack(
-          children: [
-            AnimatedBuilder(
-              animation: sizeAnimation,
-              builder: (context, child) {
-                if (progressBarAnimation.value < 200) {
+        position: widget.slideAnimation,
+        child: Visibility(
+          visible: _isVisible,
+          child: Stack(
+            children: [
+              AnimatedBuilder(
+                animation: widget.sizeAnimation,
+                builder: (context, child) {
                   return Container(
                     height: 3,
-                    width: sizeAnimation.value,
+                    width: widget.sizeAnimation.value,
                     color: Colors.grey,
                   );
-                } else {
-                  return const SizedBox();
-                }
-              },
-            ),
-            AnimatedBuilder(
-              animation: progressBarAnimation,
-              builder: (context, child) {
-                if (progressBarAnimation.value < 200) {
+                },
+              ),
+              AnimatedBuilder(
+                animation: widget.progressBarAnimation,
+                builder: (context, child) {
                   return Container(
                     height: 3,
-                    width: progressBarAnimation.value,
+                    width: widget.progressBarAnimation.value,
                     color: Colors.pink,
                   );
-                } else {
-                  return const SizedBox();
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
